902960a7936437ed48e5303196a1893312f0bc2b
nevermade <d.abarca@pucp.pe>
2015-11-10 16:08:28 -0500

    added logintest

--- app/src/androidTest/java/com/example/dp2/afiperu/common/BaseTestView.java
+++ app/src/androidTest/java/com/example/dp2/afiperu/common/BaseTestView.java
@@ -8,21 +8,21 @@ import com.example.dp2.afiperu.ui.activity.DetailActivity;
 /**
  * Created by DABARCA on 05/11/2015.
  */
-public class BaseTest extends ActivityInstrumentationTestCase2<DetailActivity> {
-    DetailActivity activity;
-    public BaseTest() {
+public class BaseTestView extends ActivityInstrumentationTestCase2<DetailActivity> {
+    public DetailActivity activity;
+    public BaseTestView() {
         super(DetailActivity.class);
+        init();
+    }
 
+    public void init(){
         try {
-            init();
+            super.setUp();
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    private void init() throws Exception{
-        super.setUp();
         injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+        activity=getActivity();
     }
 
     @Override
--- app/src/androidTest/java/com/example/dp2/afiperu/views/LoginView.java
+++ app/src/androidTest/java/com/example/dp2/afiperu/views/LoginView.java
@@ -1,8 +1,45 @@
 package com.example.dp2.afiperu.views;
 
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.action.ViewActions;
+
+import com.example.dp2.afiperu.R;
+import com.example.dp2.afiperu.common.BaseTestView;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 /**
  * Created by DABARCA on 05/11/2015.
  */
-public class LoginView {
-    
+public class LoginView  extends BaseTestView {
+    ViewInteraction txtUsername;
+    ViewInteraction txtPassword;
+    ViewInteraction btnLogin;
+
+
+    public LoginView(){
+        super();
+        txtUsername=onView(withId(R.id.login_username));
+        txtPassword=onView(withId(R.id.login_password));
+        btnLogin=onView(withId(R.id.login_btn));
+    }
+
+    public void sendUsername(String username){
+        txtUsername.perform(typeText(username), ViewActions.closeSoftKeyboard());
+    }
+
+    public void sendPassword(String password){
+        txtPassword.perform(typeText(password), ViewActions.closeSoftKeyboard());
+    }
+
+    public void clickLoginButton(){
+        btnLogin.perform(click());
+    }
+
+    public void assertLoginSuccesful(){
+        btnLogin.check(doesNotExist());
+    }
 }
